29eb46e195d3f1943d0c4c3900c37b7bacb8a27f
geek-yang <y.liu@esciencecenter.nl>
2022-04-20 23:25:54 +0200

    cpp file for insertion sorting

---
+++ fundamental/sorting/insertion_sorting.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <array>
+//using namespace std;
+
+int main()
+{
+  int number_list[10] = {11, 5, 20, 14, 39, 8, 130, 79, 12, 37};
+  std::cout << "This example shows the minimum example of insertion sorting.";
+  return 0;
+}
+
+void array_print(){
+    
+}
