dd15bed7d44b8a458806f990274be7f27a9df01b
geek-yang <y.liu@esciencecenter.nl>
2022-06-11 12:32:29 +0200

    complete basic binary search algorithm with c++

--- fundamental/binary_search/binary_search.cc
+++ fundamental/binary_search/binary_search.cc
@@ -4,10 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    // void binary_search(vector<int>& nums, int target) {
-    //     target = ;
+    int binary_search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
 
-    // }
+        return -1;
+    }
 
     void vector_print(vector<int>& nums, int size){
     for (int i = 0; i < size; i++){
@@ -19,8 +31,25 @@ public:
 
 int main()
 {
+    // first checker
     vector<int> nums {-1,0,3,5,9,12};
     int target = 9;
     Solution solution;
+    std::cout << "First test.\n";
+    std::cout << "Test data.\n";
     solution.vector_print(nums, nums.size());
+    
+    int result;
+    result = solution.binary_search(nums, target);
+    std::cout << "Output:" << result << "\n";
+
+
+    std::cout << "------------------------------\n";
+
+    // second checker
+    target = 2;
+    std::cout << "Second test.\n";
+    std::cout << "Output.\n";
+    result = solution.binary_search(nums, target);
+    std::cout << "Output:" << result << "\n";
 }
