93c24ca801088e8ee02c07591d4064a1ab50aaf9
geek-yang <y.liu@esciencecenter.nl>
2022-06-11 23:17:27 +0200

    test cases

--- fundamental/array/remove_element.cc
+++ fundamental/array/remove_element.cc
@@ -3,8 +3,34 @@
 using namespace std;
 
 class Solution {
+/* Solution for leetcode question 27 */
 public:
-    int remove_element () {
-        //something
+    int remove_element_smart (vector<int>& nums, int target) {
+        //something TBA
+        return -1;
     }
+
+    int remove_element_brute_force (vector<int>& nums, int target) {
+        return -1;
+    }
+
+    void print_input (vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            cout << nums[i] << " ";
+        }
+        cout << "\n" << endl;
+    }
+};
+
+int main () {
+    // test case 1
+    vector<int> nums{3,2,2,3};
+    int val = 3;
+
+    Solution solver;
+    solver.print_input(nums);
+
+    // test case 2
+    nums = {0,1,2,2,3,0,4,2};
+    val = 2;
 }
