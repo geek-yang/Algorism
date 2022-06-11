ce2033ec9da6464bb7cc4db991a5215ab0aebd5d
geek-yang <y.liu@esciencecenter.nl>
2022-06-12 00:03:05 +0200

    brute force implement of remove element

--- fundamental/array/remove_element.cc
+++ fundamental/array/remove_element.cc
@@ -11,7 +11,19 @@ public:
     }
 
     int remove_element_brute_force (vector<int>& nums, int target) {
-        return -1;
+        // time complexity：O(n^2)
+        // space complexity：O(1)
+        int size = nums.size();
+        for (int i = 0; i < size; i++) {
+            if (nums[i] == target) {
+                for (int j = i; j < size - 1; j++) {
+                    nums[j] = nums[j+1];
+                }
+                i--;
+                size--;
+            }
+        }
+        return size;
     }
 
     void print_input (vector<int>& nums) {
@@ -28,9 +40,21 @@ int main () {
     int val = 3;
 
     Solution solver;
+    cout << "The array for test case 1 is:" << endl;
+    solver.print_input(nums);
+
+    int result = solver.remove_element_brute_force(nums, val);
+    cout << "The length is " << result << endl;
     solver.print_input(nums);
 
     // test case 2
     nums = {0,1,2,2,3,0,4,2};
     val = 2;
+
+    cout << "The array for test case 1 is:" << endl;
+    solver.print_input(nums);
+        
+    result = solver.remove_element_brute_force(nums, val);
+    cout << "The length is " << result << endl;
+    solver.print_input(nums);
 }
