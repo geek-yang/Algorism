b762e49708b5c524523e221ba0cb723567f4a598
geek-yang <y.liu@esciencecenter.nl>
2022-06-30 23:16:32 +0200

    complete preorder traversal

--- fundamental/data_structures/binary_tree/binary_tree_traversal.cc
+++ fundamental/data_structures/binary_tree/binary_tree_traversal.cc
@@ -16,10 +16,10 @@ struct TreeNode {
 class Solution {
 public:
     void traversal (TreeNode* cur, vector<int>& vec) {
-        if (cur == NULL) return;
+        if (cur == nullptr) return;
         vec.push_back(cur->val);
         traversal(cur->left, vec);
-        traversal(cur->left, vec);
+        traversal(cur->right, vec);
     }
 
     vector<int> preorderTraversal (TreeNode *root) {
@@ -31,5 +31,26 @@ public:
 };
 
 int main () {
+    // create a sample tree
+    TreeNode* root = new TreeNode(10);
+    TreeNode* node_0 = new TreeNode(1);
+    TreeNode* node_1 = new TreeNode(2);
+    TreeNode* node_2 = new TreeNode(3);
+    TreeNode* node_3 = new TreeNode(4);
+    root->left = node_0;
+    root->right = node_3;
+    node_0->left = node_1;
+    node_0->right = node_2;
+
+    // create a vector to save the results
+
+    vector<int> tree_traversal_result;
+
+    Solution solver;
+    tree_traversal_result = solver.preorderTraversal(root);
+    for (auto &i: tree_traversal_result) {
+        cout<< i << ' ';
+    }
+
     return 0;
 }
