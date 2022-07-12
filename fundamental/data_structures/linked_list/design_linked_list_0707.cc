7fe7719d0342485b21f972079e27975c222cae00
geek-yang <y.liu@esciencecenter.nl>
2022-07-13 00:22:51 +0200

    finish designing linked list

--- fundamental/data_structures/linked_list/design_linked_list_0707.cc
+++ fundamental/data_structures/linked_list/design_linked_list_0707.cc
@@ -21,7 +21,7 @@ public:
         if (index > (_size - 1)|| index < 0) return -1;
         ListNode* cur = _dummyhead->next;
         while (index--) cur = cur->next;
-        return cur -> val;
+        return cur->val;
     }
     
     void addAtHead(int val) {
@@ -40,32 +40,36 @@ public:
     }
     
     void addAtIndex(int index, int val) {
-        if (index < _size - 1 && index >= 0) {
-            ListNode* node = new ListNode(val);
-            ListNode* cur = _dummyhead;
-            while (index--) {
-                cur = cur->next;
-            }
-            node->next = cur->next;
-            cur->next = node;
-            _size++;
+        if (index > _size) return;
+        ListNode* node = new ListNode(val);
+        ListNode* cur = _dummyhead;
+        while (index--) {
+            cur = cur->next;
         }
+        node->next = cur->next;
+        cur->next = node;
+        _size++;
     }
     
     void deleteAtIndex(int index) {
-        if (index >= _size || index < 0) {
-            return;
+        if (index >= _size || index < 0) return;
+        ListNode* cur = _dummyhead;
+        while (index--) {
+            cur = cur->next;
         }
-
+        ListNode* temp = cur->next; // to release memory
+        cur->next = cur->next->next;
+        delete temp;
+        _size--;
     }
 
     void printList() {
         ListNode* cur = _dummyhead;
-        while (cur != nullptr) {
-            cout << cur->val << ' ';
+        while (cur->next != nullptr) {
             cur = cur->next;
+            cout << cur->val << ' ';
         }
-        cout << '\n';
+        cout << endl;
     }
 
 private:
@@ -74,10 +78,33 @@ private:
 };
 
 int main () {
-    MyLinkedList* myLinkedList = new MyLinkedList();
-    myLinkedList->addAtHead(1);
-    myLinkedList->printList();
-    myLinkedList->addAtTail(3);
-    myLinkedList->printList();
-    // myLinkedList->addAtIndex(1, 2); 
+    // test case 1
+    MyLinkedList* myLinkedList_1 = new MyLinkedList();
+    myLinkedList_1->addAtHead(1);
+    myLinkedList_1->printList();
+    myLinkedList_1->addAtTail(3);
+    myLinkedList_1->printList();
+    myLinkedList_1->addAtIndex(1, 2);
+    myLinkedList_1->printList();
+    myLinkedList_1->deleteAtIndex(1);
+    myLinkedList_1->printList();
+    // test case 2
+    cout << '\n' << endl;
+    MyLinkedList* myLinkedList_2 = new MyLinkedList();
+    myLinkedList_2->addAtHead(7);
+    myLinkedList_2->printList();
+    myLinkedList_2->addAtHead(2);
+    myLinkedList_2->printList();
+    myLinkedList_2->addAtHead(1);
+    myLinkedList_2->printList();
+    myLinkedList_2->addAtIndex(3, 0);
+    myLinkedList_2->printList();
+    myLinkedList_2->deleteAtIndex(2);
+    myLinkedList_2->printList();
+    myLinkedList_2->addAtHead(6);
+    myLinkedList_2->printList();
+    myLinkedList_2->addAtTail(4);
+    myLinkedList_2->printList();
+    int result = myLinkedList_2->get(4);
+    cout << result << endl;
 }
