225e8b253e4d4456eb1b13e972f4b0340a762337
geek-yang <y.liu@esciencecenter.nl>
2022-08-01 23:25:05 +0200

    add weighted_directed_graph_list

--- fundamental/data_structures/graph/weighted_directed_graph_list.cc
+++ fundamental/data_structures/graph/weighted_directed_graph_list.cc
@@ -15,8 +15,50 @@ class Graph {
 public:
     // a vector of vectors of Pairs to represent an adjacency list
     vector<vector<Pair>> adjList;
+
+    // Graph Constructor
+    Graph(vector<Edge> const &edges, int n) {
+        // resize the vector to hold `n` elements of type vector<Edge>
+        adjList.resize(n);
+        // add edges to the directed graph
+        for (auto &edge: edges)
+        {
+            int src = edge.src;
+            int dest = edge.dest;
+            int weight = edge.weight;
+ 
+            // insert at the end
+            adjList[src].push_back(make_pair(dest, weight));
+ 
+            // uncomment the following code for undirected graph
+            // adjList[dest].push_back(make_pair(src, weight));
+        }        
+    }
+
+    void printGraph(int n) {
+        for (int i = 0; i < n; i++) {
+            for (Pair v: adjList[i]) {
+                cout << "(" << i << ", " << v.first << ", " << v.second << ") ";
+            }
+            cout << endl;
+        }
+    }
 };
 
 int main () {
+    vector<Edge> edges =
+    {
+        // (x, y, w) —> edge from `x` to `y` having weight `w`
+        {0, 1, 6}, {1, 2, 7}, {2, 0, 5}, {2, 1, 4}, {3, 2, 10}, {5, 4, 1}, {4, 5, 3}
+    };
+
+    // total number of nodes in the graph (labelled from 0 to 5)
+    int n = 6;
+
+    // construct graph
+    Graph graph(edges, n);
+
+    graph.printGraph(n);
+
     return 0;
 }
